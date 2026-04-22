b04b4583be4789fafe358e8fb5cdf827b5ec4c4e
방성재 <sungjaepang@bangseongjaeui-MacBookAir.local>
2026-04-22 12:51:40 +0900

    포폴 완성 v.1

--- event-promotion-page/js/script.js
+++ event-promotion-page/js/script.js
@@ -1,7 +1,18 @@
 
+/*
 
 // 1. 이벤트 페이지 인터랙션
 document.querySelector("button").addEventListener("click", () => {
     // console.log("버튼 클릭");
     alert("쿠폰이 다운로드되었습니다!");
 });
+
+*/
+
+const couponBtn = document.querySelector("#couponBtn")
+
+if (couponBtn) {
+  couponBtn.addEventListener("click", function () {
+    alert("쿠폰이 발급되었습니다!")
+  })
+}
